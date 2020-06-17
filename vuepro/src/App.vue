d374d8772722e99094ec3b1bb963426d4918a2c1
pandapls <932650719@qq.com>
2020-06-17 09:24:02 +0800

    更改了公共样式的调用

--- vuepro/src/App.vue
+++ vuepro/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+  	<myhead></myhead>
     <router-view/>
     <myfoot></myfoot>
   </div>
 </template>
 <script type="text/javascript">
 import myfoot from './components/myfoot'
+import myhead from './components/myhead'
 	export default{
 		components:{
-			myfoot
+			myfoot,myhead
 		}
 	}
 </script>
